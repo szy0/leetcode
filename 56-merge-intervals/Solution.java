2d1f0eb76250ddd961f4648975714e09f99b52c6
Zhanyu <1044850190@qq.com>
2020-03-17 23:37:38 +0000

    no56 solved.

--- 56-merge-intervals/Solution.java
+++ 56-merge-intervals/Solution.java
@@ -1,16 +1,13 @@
 import java.util.*;
 
 class Solution {
+    // this method works, but only for sorted intervals
     public int[][] merge(int[][] intervals) {
-        // Map<Integer, Integer[]> pairs = new TreeMap<>();
-        // for (int[] n : intervals) {
-        //     pairs.put((Integer)n[0], new Integer[]{n[0], n[1]});
-        // }
-        // int len = pairs.size();
-        // for (int i = 0; i < len - 1; i++) {
-        //     if (pairs.get)
-        // }
         int amount = intervals.length;
+        if (amount <= 1) return intervals;
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+
         List<Integer[]> copy = new ArrayList<>();
         for (int[] pair : intervals) {
             copy.add(new Integer[]{pair[0], pair[1]});
@@ -18,18 +15,22 @@ class Solution {
 
         for (int k = 0; k <= amount - 2; k++) {
             if (copy.get(k)[1] >= copy.get(k+1)[0]) {
-                copy.get(k)[1] = copy.get(k+1)[1];
+                if (copy.get(k)[1] <= copy.get(k+1)[1]) {
+                    copy.get(k)[1] = copy.get(k+1)[1];
+                }
                 copy.remove(k+1);
                 k--;
                 amount--;
             }
         }
 
-        int[][] res = new int[copy.size()][];
+        int[][] res = new int[copy.size()][2];
         for (int m = 0; m < copy.size(); m++) {
             res[m] = new int[]{copy.get(m)[0], copy.get(m)[1]};
         }
         return res;
 
     }
+
+    
 }
